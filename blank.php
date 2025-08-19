448ec11e237bc4991a29b84b142a21feded1c5a1
Mudassir Mehdi <64723522+mudassirmehdi@users.noreply.github.com>
2025-08-19 20:55:26 +0500

    Add project to git

---
+++ blank.php
@@ -0,0 +1,48 @@
+<?php require_once ('inc/header.php'); ?>
+
+<!--begin::Body-->
+	<body id="kt_app_body" data-kt-app-header-fixed-mobile="true" data-kt-app-toolbar-enabled="true" data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true" data-kt-app-sidebar-push-header="true" data-kt-app-sidebar-push-toolbar="true" class="app-default">
+		<!--begin::Theme mode setup on page load-->
+		 
+		<!--begin::App-->
+		<div class="d-flex flex-column flex-root app-root" id="kt_app_root">
+			<!--begin::Page-->
+			<div class="app-page flex-column flex-column-fluid" id="kt_app_page">
+			     
+			    <!--begin::Header-->
+				<?php require_once('inc/navbar.php'); ?>
+				<!--end::Header-->
+
+				<!--begin::Wrapper-->
+				<div class="app-wrapper flex-column flex-row-fluid" id="kt_app_wrapper">
+					 
+					<!--begin::Sidebar-->
+					<?php require_once('inc/sidebar.php'); ?>
+					<!--end::Sidebar-->
+
+                    <!--begin::Main-->
+					<div class="app-main flex-column flex-row-fluid" id="kt_app_main">
+						<!--begin::Content wrapper-->
+						<div class="d-flex flex-column flex-column-fluid">
+							<!--begin::Content-->
+							<div id="kt_app_content" class="app-content flex-column-fluid">
+								<!--begin::Content container--> 
+								<div id="kt_app_content_container" class="app-container container-fluid">
+									<div class="row">
+										<div class="col-lg-8"></div>
+										<div class="col-lg-4"></div>
+									</div>
+								</div> 
+								<!--end::Content container-->
+							</div>
+							<!--end::Content-->
+						</div>
+						<!--end::Content wrapper-->
+
+
+
+						
+
+ 
+		
+<?php require_once('inc/footer.php'); ?>
