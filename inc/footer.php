3e77cab85d74a61bfa9d585bf32f1acafb462314
Mudassir Mehdi <64723522+mudassirmehdi@users.noreply.github.com>
2025-09-04 22:44:05 +0500

    fixess

--- inc/footer.php
+++ inc/footer.php
@@ -46,17 +46,7 @@
 		<!--end::Global Javascript Bundle-->
 		<!--begin::Vendors Javascript(used for this page only)-->
 		<script src="assets/plugins/custom/fullcalendar/fullcalendar.bundle.js"></script>
-		<script src="https://cdn.amcharts.com/lib/5/index.js"></script>
-		<script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
-		<script src="https://cdn.amcharts.com/lib/5/percent.js"></script>
-		<script src="https://cdn.amcharts.com/lib/5/radar.js"></script>
-		<script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
-		<script src="https://cdn.amcharts.com/lib/5/map.js"></script>
-		<script src="https://cdn.amcharts.com/lib/5/geodata/worldLow.js"></script>
-		<script src="https://cdn.amcharts.com/lib/5/geodata/continentsLow.js"></script>
-		<script src="https://cdn.amcharts.com/lib/5/geodata/usaLow.js"></script>
-		<script src="https://cdn.amcharts.com/lib/5/geodata/worldTimeZonesLow.js"></script>
-		<script src="https://cdn.amcharts.com/lib/5/geodata/worldTimeZoneAreasLow.js"></script>
+ 
 		<script src="assets/plugins/custom/datatables/datatables.bundle.js"></script> 
 		 
 		<!--end::Vendors Javascript-->
--- my-tokens.php
+++ my-tokens.php
@@ -1,18 +1,9 @@
 <?php require_once ('inc/header.php'); ?>
 
-<!--begin::Body-->
-    <!--begin::CDN CSS-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.3/css/dataTables.bootstrap5.css">
-    <!--end::CDN CSS-->
-
+<!--begin::Body--> 
     <body id="kt_app_body" data-kt-app-header-fixed-mobile="true" data-kt-app-toolbar-enabled="true" data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true" data-kt-app-sidebar-push-header="true" data-kt-app-sidebar-push-toolbar="true" class="app-default">
         <!--begin::Theme mode setup on page load-->
-<style>
-    .form-select.form-select-solid{
-        display: none;
-    }
-</style>
+ 
         <!--begin::App-->
         <div class="d-flex flex-column flex-root app-root" id="kt_app_root">
             <!--begin::Page-->
@@ -294,20 +285,7 @@
             </div>
             <!--end::Page-->
         </div>
-        <!--end::App-->
-
-        <!--begin::CDN JS-->
-        <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
-        <script src="https://cdn.datatables.net/2.3.3/js/dataTables.js"></script>
-        <script src="https://cdn.datatables.net/2.3.3/js/dataTables.bootstrap5.js"></script>
-    <script src="assets/js/custom/my-tokens.js"></script>
-        <!--end::CDN JS-->
-
- 
-
-						
-
+        <!--end::App--> 
  
 		
 <?php require_once('inc/footer.php'); ?>
